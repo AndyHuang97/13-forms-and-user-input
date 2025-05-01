f81a8c114d6451ca4afc932a826ad958dc0293bc
Andrea Huang <huangandrea@hotmail.it>
2025-05-01 12:25:48 +0200

    Handling Form Submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,33 @@
 export default function Login() {
-  return (
-    <form>
-      <h2>Login</h2>
+  // event is given on submit
+  function handleSubmit(event) {
+    event.preventDefault(); // prevent default browser behavior of sending http request
+    console.log("Form submitted");
+  }
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div className='control-row'>
+        <div className='control no-margin'>
+          {/* the htmlFor attribute is used to link the label to the input field */}
+          <label htmlFor='email'>Email</label>
+          <input id='email' type='email' name='email' />
         </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+        <div className='control no-margin'>
+          <label htmlFor='password'>Password</label>
+          <input id='password' type='password' name='password' />
         </div>
       </div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+      <p className='form-actions'>
+        {/* by default buttons inside a form are of type submit, this sends an
+        http request to the react server and refresh the page
+        One way to solve this problem is adding type="button".
+        Another more elegant way is to use onSubmit on the form*/}
+        <button className='button button-flat'>Reset</button>
+        <button className='button' /*onClick={handleSubmit}*/>Login</button>
       </p>
     </form>
   );
